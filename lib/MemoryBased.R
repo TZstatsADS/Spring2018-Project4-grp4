4a4c0bfa6aaf113203982df2e408b803cbda944c
ecolalala <35772736+ecolalala@users.noreply.github.com>
2018-04-10 16:54:15 -0400

    ms

--- lib/MemoryBased.R
+++ lib/MemoryBased.R
@@ -1,3 +1,26 @@
+##Transform Data MS
+Transform_ms <- function(data){
+  case_line <- which(data$V1 == 'C')
+  user_id <- data$V3[case_line]
+  vote_id <- sort(unique(data$V2[which(data$V1 == 'V')]))
+  table <- matrix(0, nrow = length(user_id), ncol = length(vote_id))
+  for(i in 1:length(case_line)){
+    start_num <- case_line[i]
+    if(i < length(case_line)){
+      end_num <- case_line[i+1]
+    }else{
+      end_num <- nrow(data)+1
+    }
+    for(index in (start_num+1):(end_num-1)){
+      j <- which(vote_id == data[index,3])
+      table[i,j] <- 1
+    }
+  }
+  rownames(table) <- as.character(user_id)
+  colnames(table) <- as.character(vote_id)
+  return(table)
+}
+
 ##Transform movie data
 Transform_m <- function(data){
   rows <- sort(unique(data$User))
@@ -15,3 +38,5 @@ Transform_m <- function(data){
   colnames(table) <- columns
   return(table)
 }
+
+
